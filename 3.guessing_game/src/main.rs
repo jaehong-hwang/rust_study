6f860654b4175f79d0179c442f66380c4d1018e1
Jaehong Hwang <dev.jaehong@gmail.com>
2022-04-30 17:01:29 +0900

    tab size 2 -> 4

--- 3.guessing_game/src/main.rs
+++ 3.guessing_game/src/main.rs
@@ -5,36 +5,36 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-  println!("Guess the number!");
+    println!("Guess the number!");
 
-  let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-  println!("The secret number is: {}", secret_number);
+    println!("The secret number is: {}", secret_number);
 
-  loop {
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // mut는 가변 여부
-    // let guess = String::new(); 일 경우 가변이 안되는 상수
-    let mut guess = String::new();
+        // mut는 가변 여부
+        // let guess = String::new(); 일 경우 가변이 안되는 상수
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = match guess.trim().parse() {
-      Ok(num) => num,
-      Err(_) => continue,
-    };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal   => {
-          println!("You win!");
-          break;
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
         }
     }
-  }
 }
